8ddf24e4d2c016f20a0e0393b1793d104df4d88e
Daniel Høyer Iversen <daniel.h.iversen@ntnu.no>
2018-01-12 14:57:40 +0100

    readd

--- PyXiaomiGateway/__init__.py
+++ PyXiaomiGateway/__init__.py
@@ -337,6 +337,8 @@ class XiaomiGateway(object):
         encryptor = Cipher(algorithms.AES(self.key.encode()), modes.CBC(init_vector),
                            backend=default_backend()).encryptor()
         ciphertext = encryptor.update(self.token.encode()) + encryptor.finalize()
+        if isinstance(ciphertext, str):  # For Python 2 compatibility
+            return ''.join('{:02x}'.format(ord(x)) for x in ciphertext)
         return ''.join('{:02x}'.format(x) for x in ciphertext)
 
 
