a12fc7a0764c85c9366ca7f3fd040db360801d50
Kerwin Bryant <ileler@qq.com>
2018-03-30 14:55:13 +0800

    support new device model;support new LAN protocol 2.0 (#66)
    * support new device model;support new LAN protocol 2.0
    
    * format code style
    
    * optimization code
    
    * optimization code

--- xiaomi_gateway/__init__.py
+++ xiaomi_gateway/__init__.py
@@ -11,6 +11,8 @@ from cryptography.hazmat.backends import default_backend
 
 _LOGGER = logging.getLogger(__name__)
 
+GATEWAY_MODELS = ['gateway', 'gateway.v3', 'acpartner.v3']
+
 
 class XiaomiGatewayDiscovery(object):
     """PyXiami."""
@@ -60,7 +62,7 @@ class XiaomiGatewayDiscovery(object):
                     sid, ip_address, port)
 
                 self.gateways[ip_address] = XiaomiGateway(
-                    ip_address, port, sid, key, self._socket)
+                    ip_address, port, sid, key, self._socket, gateway.get('proto'))
             except OSError as error:
                 _LOGGER.error(
                     "Could not resolve %s: %s", host, error)
@@ -79,7 +81,7 @@ class XiaomiGatewayDiscovery(object):
                     _LOGGER.error("Response does not match return cmd")
                     continue
 
-                if resp["model"] != 'gateway':
+                if resp["model"] not in GATEWAY_MODELS:
                     _LOGGER.error("Response must be gateway model")
                     continue
 
@@ -98,7 +100,8 @@ class XiaomiGatewayDiscovery(object):
 
                 _LOGGER.info('Xiaomi Gateway %s found at IP %s', sid, ip_add)
 
-                self.gateways[ip_add] = XiaomiGateway(ip_add, port, sid, gateway_key, self._socket)
+                self.gateways[ip_add] = XiaomiGateway(ip_add, port, sid, gateway_key, self._socket,
+                                                      resp["proto_version"] if "proto_version" in resp else None)
 
         except socket.timeout:
             _LOGGER.info("Gateway discovery finished in 5 seconds")
@@ -166,7 +169,7 @@ class XiaomiGatewayDiscovery(object):
                     continue
 
                 cmd = data['cmd']
-                if cmd == 'heartbeat' and data['model'] == 'gateway':
+                if cmd == 'heartbeat' and data['model'] in GATEWAY_MODELS:
                     gateway.token = data['token']
                 elif cmd == 'report' or cmd == 'heartbeat':
                     _LOGGER.debug('MCAST (%s) << %s', cmd, data)
@@ -184,7 +187,7 @@ class XiaomiGateway(object):
     """Xiaomi Gateway Component"""
 
     # pylint: disable=too-many-arguments
-    def __init__(self, ip_adress, port, sid, key, sock):
+    def __init__(self, ip_adress, port, sid, key, sock, proto=None):
 
         self.ip_adress = ip_adress
         self.port = int(port)
@@ -195,6 +198,12 @@ class XiaomiGateway(object):
         self.token = None
         self._socket = sock
 
+        if proto is None:
+            cmd = '{"cmd":"read","sid":"' + sid + '"}'
+            resp = self._send_cmd(cmd)
+            proto = _get_value(resp, "proto_version") if _validate_data(resp) else None
+        self.proto = '1.0' if proto is None else proto
+
         trycount = 5
         for _ in range(trycount):
             _LOGGER.info('Discovering Xiaomi Devices')
@@ -203,38 +212,47 @@ class XiaomiGateway(object):
 
     def _discover_devices(self):
 
-        cmd = '{"cmd" : "get_id_list"}'
-        resp = self._send_cmd(cmd, "get_id_list_ack")
-        if resp is None or "token" not in resp or "data" not in resp:
+        cmd = '{"cmd" : "get_id_list"}' if int(self.proto[0:1]) == 1 else '{"cmd":"discovery"}'
+        resp = self._send_cmd(cmd, "get_id_list_ack") if int(self.proto[0:1]) == 1 \
+            else self._send_cmd(cmd, "discovery_rsp")
+        if resp is None or "token" not in resp or ("data" not in resp and "dev_list" not in resp):
             return False
         self.token = resp['token']
-        sids = json.loads(resp["data"])
+        sids = []
+        if int(self.proto[0:1]) == 1:
+            sids = json.loads(resp["data"])
+        else:
+            for dev in resp["dev_list"]:
+                sids.append(dev["sid"])
         sids.append(self.sid)
 
         _LOGGER.info('Found %s devices', len(sids))
 
         device_types = {
-            'sensor': ['sensor_ht', 'gateway', 'weather.v1', 'sensor_motion.aq2'],
-            'binary_sensor': ['magnet', 'sensor_magnet.aq2',
-                              'motion', 'sensor_motion.aq2',
-                              'switch', 'sensor_switch.aq2', 'sensor_switch.aq3',
-                              '86sw1', '86sw2', 'cube', 'smoke', 'natgas', 'sensor_wleak.aq1'],
+            'sensor': ['sensor_ht', 'gateway', 'gateway.v3', 'weather',
+                       'weather.v1', 'sensor_motion.aq2', 'acpartner.v3'],
+            'binary_sensor': ['magnet', 'sensor_magnet', 'sensor_magnet.aq2',
+                              'motion', 'sensor_motion', 'sensor_motion.aq2',
+                              'switch', 'sensor_switch', 'sensor_switch.aq2', 'sensor_switch.aq3',
+                              '86sw1', 'sensor_86sw1.aq1',
+                              '86sw2', 'sensor_86sw2.aq1',
+                              'cube', 'sensor_cube',
+                              'smoke', 'sensor_smoke',
+                              'natgas', 'sensor_natgas',
+                              'sensor_wleak.aq1'],
             'switch': ['plug',
                        'ctrl_neutral1', 'ctrl_neutral1.aq1',
                        'ctrl_neutral2', 'ctrl_neutral2.aq1',
                        'ctrl_ln1', 'ctrl_ln1.aq1',
                        'ctrl_ln2', 'ctrl_ln2.aq1',
-                       '86plug', 'ctrl_86plug.aq1'],
-            'light': ['gateway'],
+                       '86plug', 'ctrl_86plug', 'ctrl_86plug.aq1'],
+            'light': ['gateway', 'gateway.v3'],
             'cover': ['curtain']}
 
         for sid in sids:
             cmd = '{"cmd":"read","sid":"' + sid + '"}'
-            resp = self._send_cmd(cmd, "read_ack")
-            if resp is None:
-                continue
-            data = json.loads(resp["data"])
-            if "error" in data:
+            resp = self._send_cmd(cmd, "read_ack") if int(self.proto[0:1]) == 1 else self._send_cmd(cmd, "read_rsp")
+            if not _validate_data(resp):
                 _LOGGER.error("Not a valid device. Check the mac adress and update the firmware.")
                 continue
 
@@ -246,9 +264,10 @@ class XiaomiGateway(object):
                     supported = True
                     xiaomi_device = {
                         "model": model,
+                        "proto": self.proto,
                         "sid": resp["sid"],
-                        "short_id": resp["short_id"],
-                        "data": data,
+                        "short_id": resp["short_id"] if "short_id" in resp else 0,
+                        "data": _list2map(_get_value(resp)),
                         "raw_data": resp}
                     self.devices[device_type].append(xiaomi_device)
                     _LOGGER.debug('Registering device %s, %s as: %s', sid, model, device_type)
@@ -269,7 +288,7 @@ class XiaomiGateway(object):
                 continue
         return True
 
-    def _send_cmd(self, cmd, rtn_cmd):
+    def _send_cmd(self, cmd, rtn_cmd=None):
         try:
             self._socket.settimeout(10.0)
             _LOGGER.debug("_send_cmd >> %s", cmd.encode())
@@ -283,7 +302,7 @@ class XiaomiGateway(object):
             return None
         resp = json.loads(data.decode())
         _LOGGER.debug("_send_cmd resp << %s", resp)
-        if resp['cmd'] != rtn_cmd:
+        if rtn_cmd is not None and resp['cmd'] != rtn_cmd:
             _LOGGER.error("Non matching response. Expecting %s, but got %s", rtn_cmd, resp['cmd'])
             return None
         return resp
@@ -299,37 +318,48 @@ class XiaomiGateway(object):
         if not self.token:
             _LOGGER.debug('Gateway Token was not obtained yet. Cannot send commands to the gateway.')
             return False
-        data['key'] = self._get_key()
+
         cmd = dict()
         cmd['cmd'] = 'write'
         cmd['sid'] = sid
-        cmd['data'] = data
-        resp = self._send_cmd(json.dumps(cmd), "write_ack")
+        if int(self.proto[0:1]) == 1:
+            data['key'] = self._get_key()
+            cmd['data'] = data
+        else:
+            cmd['key'] = self._get_key()
+            cmd['params'] = [data]
+        resp = self._send_cmd(json.dumps(cmd), "write_ack") if int(self.proto[0:1]) == 1 \
+            else self._send_cmd(json.dumps(cmd), "write_rsp")
         _LOGGER.debug("write_ack << %s", resp)
         if _validate_data(resp):
             return True
-        if (resp is None or 'data' not in resp or 'error' not in resp['data'] or
-                'Invalid key' not in resp['data']):
+        if not _validate_keyerror(resp):
             return False
 
         # If 'invalid key' message we ask for a new token
-        resp = self._send_cmd('{"cmd" : "get_id_list"}', "get_id_list_ack")
+        resp = self._send_cmd('{"cmd" : "get_id_list"}', "get_id_list_ack") if int(self.proto[0:1]) == 1 \
+            else self._send_cmd('{"cmd" : "discovery"}', "discovery_rsp")
         _LOGGER.debug("get_id_list << %s", resp)
 
         if resp is None or "token" not in resp:
             _LOGGER.error('No new token from gateway. Can not send commands to the gateway.')
             return False
         self.token = resp['token']
-        data['key'] = self._get_key()
-        cmd['data'] = data
-        resp = self._send_cmd(json.dumps(cmd), "write_ack")
+        if int(self.proto[0:1]) == 1:
+            data['key'] = self._get_key()
+            cmd['data'] = data
+        else:
+            cmd['key'] = self._get_key()
+            cmd['params'] = [data]
+        resp = self._send_cmd(json.dumps(cmd), "write_ack") if int(self.proto[0:1]) == 1 \
+            else self._send_cmd(json.dumps(cmd), "write_rsp")
         _LOGGER.debug("write_ack << %s", resp)
         return _validate_data(resp)
 
     def get_from_hub(self, sid):
         """Get data from gateway"""
         cmd = '{ "cmd":"read","sid":"' + sid + '"}'
-        resp = self._send_cmd(cmd, "read_ack")
+        resp = self._send_cmd(cmd, "read_ack") if int(self.proto[0:1]) == 1 else self._send_cmd(cmd, "read_rsp")
         _LOGGER.debug("read_ack << %s", resp)
         return self.push_data(resp)
 
@@ -337,7 +367,7 @@ class XiaomiGateway(object):
         """Push data broadcasted from gateway to device"""
         if not _validate_data(data):
             return False
-        jdata = json.loads(data['data'])
+        jdata = json.loads(data['data']) if int(self.proto[0:1]) == 1 else _list2map(data['params'])
         if jdata is None:
             return False
         sid = data['sid']
@@ -357,10 +387,50 @@ class XiaomiGateway(object):
 
 
 def _validate_data(data):
-    if data is None or "data" not in data:
+    if data is None or ("data" not in data and "params" not in data):
         _LOGGER.error('No data in response from hub %s', data)
         return False
-    if 'error' in data['data']:
+    if "data" in data and 'error' in json.loads(data['data']):
         _LOGGER.error('Got error element in data %s', data['data'])
         return False
+    if "params" in data:
+        for param in data['params']:
+            if 'error' in param:
+                _LOGGER.error('Got error element in data %s', data['params'])
+                return False
     return True
+
+
+def _validate_keyerror(data):
+    if data is not None and "data" in data and 'Invalid key' in data['data']:
+        return True
+    if data is not None and "params" in data:
+        for param in data['params']:
+            if 'error' in param and 'Invalid key' in param['error']:
+                return True
+    return False
+
+
+def _get_value(resp, data_key=None):
+    if not _validate_data(resp):
+        return None
+    data = json.loads(resp["data"]) if "data" in resp else resp["params"]
+    if data_key is None:
+        return data
+    if isinstance(data, list):
+        for param in data:
+            if data_key in param:
+                return param[data_key]
+        return None
+    return data[data_key]
+
+
+def _list2map(data):
+    if not isinstance(data, list):
+        return data
+    new_data = {}
+    for obj in data:
+        for key in obj:
+            new_data[key] = obj[key]
+    new_data['raw_data'] = data
+    return new_data
