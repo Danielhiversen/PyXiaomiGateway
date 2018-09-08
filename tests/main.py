47c971651090ef2f75d5d00b1618b343180a92f3
Paul Annekov <paul.annekov@gmail.com>
2018-09-08 19:22:57 +0300

    checking travis network config

--- tests/main.py
+++ tests/main.py
@@ -8,22 +8,23 @@ from concurrent.futures import ThreadPoolExecutor
 # from xiaomi_gateway import XiaomiGatewayDiscovery
 
 class DiscoveryProtocol(asyncio.DatagramProtocol):
-    def __init__(self):
+    def __init__(self, ip):
         super().__init__()
+        self.ip = ip
     def connection_made(self, transport):
-        print('Discovery connected')
+        print('Discovery connected', self.ip)
         self.transport = transport
     def datagram_received(self, data, addr):
         req = json.loads(data.decode())
-        print('Discovery received:', req)
+        print('Discovery', self.ip, 'received:', req, addr)
         res = json.dumps({
             "cmd": "iam",
-            "ip": "127.0.0.1",
+            "ip": self.ip,
             "port": "9898",
             "model": "gateway",
             "sid": "1232",
         })
-        print('Discovery sent:', res)
+        print('Discovery', self.ip, 'sent:', res)
         self.transport.sendto(res.encode(), addr)
         self.transport.close()
 
@@ -42,6 +43,7 @@ class ServerProtocol(asyncio.DatagramProtocol):
 class ClientProtocol:
     def __init__(self):
         self.transport = None
+        self.res_count = 0
 
     def connection_made(self, transport):
         print('Client connected')
@@ -55,21 +57,23 @@ class ClientProtocol:
         loop.stop()
 
     def datagram_received(self, data, addr):
-        print("Client received:", data.decode())
-        self.transport.close()
+        print("Client received:", data.decode(), addr)
+        self.res_count = self.res_count+1
+        if self.res_count==1:
+            self.transport.close()
 
-def start_server(loop):
+def start_server(loop, ip):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(('224.0.0.51', 4321))
-    mreq = struct.pack("4sl", socket.inet_aton('224.0.0.51'), socket.INADDR_ANY)
+    mreq = socket.inet_aton('224.0.0.51') + socket.inet_aton(ip)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-    t = loop.create_datagram_endpoint(DiscoveryProtocol,
+    t = loop.create_datagram_endpoint(lambda: DiscoveryProtocol(ip),
                                       sock=sock)
     loop.run_until_complete(t)
     t = loop.create_datagram_endpoint(ServerProtocol,
-                                      local_addr=('0.0.0.0', 9898))
+                                      local_addr=(ip, 9898))
     loop.run_until_complete(t)
 
 def callback():
@@ -85,6 +89,7 @@ def start_client(loop, pool):
 logging.basicConfig(level=logging.DEBUG)
 pool = ThreadPoolExecutor(max_workers=multiprocessing.cpu_count())
 loop = asyncio.get_event_loop()
-start_server(loop)
+start_server(loop, '0.0.0.0')
+# start_server(loop, '0.0.0.0')
 start_client(loop, pool)
 loop.run_forever()
