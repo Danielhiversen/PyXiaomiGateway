da917ad484115d5cacd9f545f46ed48bac9bad46
Paul Annekov <paul.annekov@gmail.com>
2018-09-23 14:35:44 +0300

    code style, namings, better 'key' check, handle bad requests, moved protocols init to gateway.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,43 +2,31 @@
 import pytest
 import socket
 import asyncio
-from tests.gateway import DiscoveryProtocol, ServerProtocol
+from tests.gateway import AqaraGateway
 from xiaomi_gateway import XiaomiGatewayDiscovery
 
+
 @pytest.yield_fixture
-def server_factory(event_loop):
-    protocols = []
-    def start_server(server):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(('224.0.0.50', 4321))
-        mreq = socket.inet_aton('224.0.0.50') + socket.inet_aton(server['ip'])
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-        discovery_protocol = DiscoveryProtocol(server)
-        t = event_loop.create_datagram_endpoint(lambda: discovery_protocol,
-                                                sock=sock)
-        asyncio.ensure_future(t, loop=event_loop)
-        server_protocol = ServerProtocol(server)
-        t = event_loop.create_datagram_endpoint(lambda: server_protocol,
-                                        local_addr=(server['ip'], 9898))
-        asyncio.ensure_future(t, loop=event_loop)
-        protocols.append(discovery_protocol)
-        protocols.append(server_protocol)
-    
-    yield start_server
-    [protocol.stop() for protocol in protocols]
+def gateway_factory(event_loop):
+    """Factory that creates new gateways"""
+    gateways = []
+
+    def start_gateway(config):
+        gateways.append(AqaraGateway(config, event_loop))
+
+    yield start_gateway
+    [gateway.stop() for gateway in gateways]
+
 
 @pytest.yield_fixture
 def client_factory():
+    """Factory that creates new gateway clients"""
     clients = []
+
     def start_client(ip, gateways):
-        client = XiaomiGatewayDiscovery(lambda: None, [{
-            'key': gateways[0]['key'],
-            'sid': gateways[0]['sid'],
-        }, {
-            'key': gateways[1]['key'],
-            'sid': gateways[1]['sid'],
-        }], ip)
+        config = []
+        [config.append({'key': g['key'], 'sid': g['sid']}) for g in gateways]
+        client = XiaomiGatewayDiscovery(lambda: None, config, ip)
         clients.append(client)
         return client
 
--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -6,11 +6,10 @@ import multiprocessing
 import struct
 import pytest
 from concurrent.futures import ThreadPoolExecutor
-from xiaomi_gateway import XiaomiGatewayDiscovery
 
 _LOGGER = logging.getLogger(__name__)
 
-gateway = {
+dev_gateway = {
     'model': 'gateway',
     'data': {
         "rgb": 0,
@@ -19,18 +18,18 @@ gateway = {
     }
 }
 
-plug = {
+dev_plug = {
     'model': 'plug',
     'data': {
         "voltage": 3600,
         "status": "off",
         "inuse": "0",
-        "power_consumed": "38344",
+        "power_consumed": "12345",
         "load_power": "0.00"
     }
 }
 
-magnet = {
+dev_magnet = {
     'model': 'magnet',
     'data': {
         "voltage": 3035,
@@ -38,62 +37,70 @@ magnet = {
     }
 }
 
-server1 = {
+gateway1 = {
     'ip': '10.0.0.2',
     'sid': '1',
     'key': 'a6c567lbkcmr47fp',
     'devices': {
-        '1': dict({'sid': '1', 'short_id': 0}, **gateway),
-        '2': dict({'sid': '2', 'short_id': 20}, **magnet),
+        '1': dict({'sid': '1', 'short_id': 0}, **dev_gateway),
+        '2': dict({'sid': '2', 'short_id': 20}, **dev_magnet),
     },
 }
 
-server2 = {
+gateway2 = {
     'ip': '10.0.0.3',
-    'sid': '3', 
-    'key': 'c6c36albocvr97fl', 
+    'sid': '3',
+    'key': 'c6c36albocvr97fl',
     'devices': {
-        '3': dict({'sid': '3', 'short_id': 0}, **gateway),
-        '4': dict({'sid': '4', 'short_id': 40}, **plug),
+        '3': dict({'sid': '3', 'short_id': 0}, **dev_gateway),
+        '4': dict({'sid': '4', 'short_id': 40}, **dev_plug),
     },
 }
+
 
 @pytest.yield_fixture(autouse=True)
 def debug_log(caplog):
+    """Asserts logs are lower than warning"""
     caplog.set_level(logging.DEBUG)
     yield
     for record in caplog.get_records('call'):
-        assert record.levelno<logging.WARNING
+        assert record.levelno < logging.WARNING
+
 
 @pytest.fixture(autouse=True)
-def servers(server_factory):
-    server_factory(server1)
-    server_factory(server2)
+def gateways(gateway_factory):
+    """Automatically creates 2 gateways for each test"""
+    gateway_factory(gateway1)
+    gateway_factory(gateway2)
+
 
 @pytest.fixture
 def pool():
+    """Returns thread pool for sync calls"""
     return ThreadPoolExecutor(max_workers=multiprocessing.cpu_count())
+
 
 @pytest.mark.asyncio
 async def test_simple(event_loop, pool, client_factory):
-    client = client_factory('10.0.0.1', [server1, server2])
+    """2 gateways discovery -> read gateway #1 -> write gateway #2"""
+    client = client_factory('10.0.0.1', [gateway1, gateway2])
     await event_loop.run_in_executor(pool, client.discover_gateways)
-    ok = await event_loop.run_in_executor(pool,
-        client.gateways['10.0.0.2'].get_from_hub, '2')
+    ok = await event_loop.run_in_executor(pool, client.gateways[gateway1['ip']].get_from_hub, '2')
     assert ok
-    ok = await event_loop.run_in_executor(pool,
-        lambda: client.gateways['10.0.0.3'].write_to_hub('4', status='on'))
+    ok = await event_loop.run_in_executor(pool, lambda: client.gateways[gateway2['ip']].write_to_hub('4', status='on'))
     assert ok
+
 
 @pytest.mark.asyncio
 async def test_race(event_loop, pool, client_factory):
-    client = client_factory('10.0.0.1', [server1, server2])
+    """2 gateways discovery -> 100 x (read gateway #1 + write gateway #2)
+    https://github.com/Danielhiversen/PyXiaomiGateway/issues/45
+    """
+    client = client_factory('10.0.0.1', [gateway1, gateway2])
     await event_loop.run_in_executor(pool, client.discover_gateways)
     for i in range(100):
-        task1 = event_loop.run_in_executor(pool,
-            client.gateways['10.0.0.2'].get_from_hub, '2')
-        task2 = event_loop.run_in_executor(pool,
-            lambda: client.gateways['10.0.0.3'].write_to_hub('4', status='on'))
+        task1 = event_loop.run_in_executor(pool, client.gateways[gateway1['ip']].get_from_hub, '2')
+        task2 = event_loop.run_in_executor(pool, lambda: client.gateways[gateway2['ip']].write_to_hub('4', status='on'))
         res = await asyncio.gather(task1, task2)
         assert res[0], "failed on get_from_hub in %i lap" % i
         assert res[1], "failed on write_to_hub in %i lap" % i
